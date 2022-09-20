2d8644fdfe67cf06ea1e1442bd5328369f579014
Vikneysh Raj G G A <65613787+Vikneysh@users.noreply.github.com>
2022-09-20 15:23:51 +0530

    Committing Assignment 2

--- Assignments/Team Member 3 Vikneysh Raj G G A/Assignment 02/Program3.py
+++ Assignments/Team Member 3 Vikneysh Raj G G A/Assignment 02/Program3.py
@@ -1,16 +1,20 @@
+
+def primecheck(num):
+    flag = False
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = True
+                break
+        return flag
+        
+    
 def main():
-    n=int(input("Enter the value of n: "))
-    a=-1
-    b=1
-    print("Fibonacci Series:\n")
-    i=0
-    while(i<n):
-        t=a+b
-        a=b
-        b=t
-        print(t)        
-        i+=1
-
-
+    n = int(input("Enter the number: "))
+    for num in range(0,n):
+        flag=primecheck(num)
+        if flag==False:
+            print(num)
+    
 if __name__ == "__main__":
     main()
