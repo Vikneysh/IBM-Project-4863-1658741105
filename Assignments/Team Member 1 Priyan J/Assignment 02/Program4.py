80915e4efc5999c415fc0cc06160b45d18edfaf7
Priyan Jeyaram <impriyanj@gmail.com>
2022-09-20 14:03:15 +0530

    Committing Assignment 2

---
+++ Assignments/Team Member 1 Priyan J/Assignment 02/Program4.py
@@ -0,0 +1,9 @@
+#Wite a python program to generate Fibonacci series.
+
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+if __name__ == "__main__":
+    n = 9
+    print(fib(n))
