eec4cc19e73f679f560bca8b19dfc8cc72e86e5d
Jebas Winston <jebaswinston55@gmail.com>
2022-10-11 23:01:55 +0530

    Updating Assignment

--- Assessments/Team Leader Manikantan R V/Assignment - Python/Program1.py
+++ Assessments/Team Leader Manikantan R V/Assignment - Python/Program1.py
@@ -1,3 +1,3 @@
 #Write a python prg to test a given number is prime or not.
-number= int(input("Enter a number : "))
+n = int(input("Enter a number : "))
 print("Its a Prime Number") if( n != 1 and 0 not in [n%i for i in range(2,n//2+1)]) else print("Not a Prime")
